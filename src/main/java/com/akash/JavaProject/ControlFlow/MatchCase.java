044d7e233df053f9e138c09c6ef5a59319bf3c7f
Akash chaudhary <akash0005007@gmail.com>
2023-01-03 11:11:27 +0530

    first ommit

---
+++ src/main/java/com/akash/JavaProject/ControlFlow/MatchCase.java
@@ -0,0 +1,16 @@
+package ControlFlow;
+
+public class MatchCase {
+    public void matchcase(int n){
+        switch(n){
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+        }
+
+    }
+
+}
---
+++ src/main/java/com/akash/JavaProject/Variables/Integer.java
@@ -0,0 +1,13 @@
+package Variables;
+
+public class Integer {
+    int i;
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+}
