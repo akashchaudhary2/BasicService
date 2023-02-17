13736ffe4b7dff9043355c19aba11452c85d6369
Akash chaudhary <akash0005007@gmail.com>
2023-02-17 15:02:26 +0530

    ch

---
+++ src/main/java/com/akash/basicService/controller/controller.java
@@ -0,0 +1,12 @@
+package com.akash.basicService.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class controller {
+    @GetMapping()
+    public String test(){
+        return "Hello web world!";
+    }
+}
